86c3206dd643c79d69591d817b580ed5a5e899ae
tina-pina <christina.koenig.de@gmail.com>
2019-01-15 20:15:09 +0100

    created Grid for games

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import static java.util.Arrays.asList;
 
@@ -52,7 +50,7 @@ public class SalvoApplication {
 			gameRepo.save(g6);
 
 			GamePlayer GP1 = new GamePlayer(g1, p1);
-			GamePlayer GP2 = new GamePlayer(g1, p2);
+			GamePlayer GP2 = new GamePlayer(g2, p2);
 			GamePlayer GP3 = new GamePlayer(g2, p1);
 			GamePlayer GP4 = new GamePlayer(g2, p2);
 			GamePlayer GP5 = new GamePlayer(g3, p2);
@@ -76,27 +74,27 @@ public class SalvoApplication {
 			gamePlayerRepo.save(GP11);
 
 			Ship S1 = new Ship("Aircraft Carrier", new ArrayList<String>(asList("B5", "B6", "B7", "B8", "B9")), GP1);
-			Ship S2 = new Ship("Battleship", new ArrayList<String>(asList("D4", "D5", "D6", "D7")) , GP1);
-			Ship S3 = new Ship("Submarine", new ArrayList<String>(asList("F3", "G3", "H3")) , GP1);
-			Ship S4 = new Ship("Destroyer", new ArrayList<String>(asList("F5", "G5", "H5")) , GP1);
-			Ship S5 = new Ship("Patrol Boat", new ArrayList<String>(asList("I8", "I9")) , GP1);
+//			Ship S2 = new Ship("Battleship", new ArrayList<String>(asList("D4", "D5", "D6", "D7")) , GP1);
+//			Ship S3 = new Ship("Submarine", new ArrayList<String>(asList("F3", "G3", "H3")) , GP1);
+//			Ship S4 = new Ship("Destroyer", new ArrayList<String>(asList("F5", "G5", "H5")) , GP1);
+//			Ship S5 = new Ship("Patrol Boat", new ArrayList<String>(asList("I8", "I9")) , GP1);
 
 			Ship S11 = new Ship("Aircraft Carrier", new ArrayList<String>(asList("C2", "C3", "C4", "C5", "C6")) , GP2);
-			Ship S12 = new Ship("Battleship", new ArrayList<String>(asList("E2", "E3", "E4", "E5")) , GP2);
-			Ship S13 = new Ship("Submarine", new ArrayList<String>(asList("G2", "H2", "I2")) , GP2);
-			Ship S14 = new Ship("Destroyer", new ArrayList<String>(asList("G7", "G8", "G9")) , GP2);
-			Ship S15 = new Ship("Patrol Boat", new ArrayList<String>(asList("J9", "J10")) , GP2);
+//			Ship S12 = new Ship("Battleship", new ArrayList<String>(asList("E2", "E3", "E4", "E5")) , GP2);
+//			Ship S13 = new Ship("Submarine", new ArrayList<String>(asList("G2", "H2", "I2")) , GP2);
+//			Ship S14 = new Ship("Destroyer", new ArrayList<String>(asList("G7", "G8", "G9")) , GP2);
+//			Ship S15 = new Ship("Patrol Boat", new ArrayList<String>(asList("J9", "J10")) , GP2);
 
 			shipRepository.save(S1);
-			shipRepository.save(S2);
-			shipRepository.save(S3);
-			shipRepository.save(S4);
-			shipRepository.save(S5);
+//			shipRepository.save(S2);
+//			shipRepository.save(S3);
+//			shipRepository.save(S4);
+//			shipRepository.save(S5);
 			shipRepository.save(S11);
-			shipRepository.save(S12);
-			shipRepository.save(S13);
-			shipRepository.save(S14);
-			shipRepository.save(S15);
+//			shipRepository.save(S12);
+//			shipRepository.save(S13);
+//			shipRepository.save(S14);
+//			shipRepository.save(S15);
 
 		};
 	}
--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,6 +1,5 @@
 
 
-
 function paramObj(search) {
   var obj = {};
   var keyRes = " ";
@@ -17,22 +16,60 @@ function paramObj(search) {
   return keyRes;
 }
 
-//console.log(paramObj(location.href));
 
-let gpPlayerId = paramObj(location.href);
+let gamePlayerId = paramObj(location.href);
+console.log(gamePlayerId)
+
+let url = (`/api/game_view/${gamePlayerId}`)
+
+fetch(url).then(res => res.json())
+.then(json => {
+    let shipsArr = json.ships;
+
+    let shipLocations = []
+    for(let ship of shipsArr) {
+        for(let loc of ship.locations) shipLocations.push(loc);
+    }
+    console.log(shipLocations);
+    for(let locationCode of shipLocations) {
+        let targetGrid = locationCode;
+        console.log(targetGrid);
+        let tableId = document.querySelectorAll(`#${targetGrid}`);
+        let singleElement = tableId[0];
+        //todo give each ship a different color;
+        singleElement.style.backgroundColor = 'yellow';
+    }
+})
 
 
-//let url = '/web/game.html?gp=${gpPlayerId}';
+let grid = document.getElementById("grid");
+let table = document.createElement("table");
+let colNames = " ABCDEFGHIJ";
 
-let url =  '/web/game.html?gp='+gpPlayerId;
-header = {'Content-Type': 'application/json',
-          'Accept': 'application/json'}
+for(let i=0; i< 11; i++) {
+    let row = document.createElement("tr");
+    for(let j=0; j<11; j++) {
+        let col = document.createElement("td");
+        if(i === 0 && j > 0) col.innerHTML = `${j}`;  // first row
+        if(j === 0 && i > 0) col.innerHTML = colNames[i];  // first item
+        col.id = colNames[i] + j;
+        row.appendChild(col);
+    }
+    table.appendChild(row);
+}
+grid.appendChild(table);
 
 
-fetch(url, {headers: header})
-//.then(response => response.json())
-.then(res => res.text())
-.then(text => console.log(text))
+
+
+
+
+
+
+
+
+
+
 
 
 
