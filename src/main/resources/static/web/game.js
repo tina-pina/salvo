9f39957e06fa432f4318b2d2c8cf6babe4d44bc7
tina-pina <christina.koenig.de@gmail.com>
2019-01-26 22:47:35 +0100

    add helper functions for ship placement

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -141,8 +141,8 @@ function createGrids() {
 
     //add ships grid
 
-    let grid = document.getElementById("shipsGrid");
-    let table = document.createElement("table");
+    let table = document.getElementById("shipsGrid");
+//    let table = document.createElement("table");
     let colNames = " ABCDEFGHIJ";
 
     for(let i=0; i< 11; i++) {
@@ -153,15 +153,17 @@ function createGrids() {
             if(j === 0 && i > 0) col.innerHTML = colNames[i];  // first item
             col.id = colNames[i] + j;
             row.appendChild(col);
+//            col.style = "position: sticky";
+//            row.style = "position: sticky";
         }
         table.appendChild(row);
     }
-    grid.appendChild(table);
+//    table.appendChild(row)
 
     //add salvos grid
 
-    grid = document.getElementById("salvosGrid");
-    table = document.createElement("table");
+    table = document.getElementById("salvosGrid");
+//    table = document.createElement("table");
     colNames = " ABCDEFGHIJ";
 
     for(let i=0; i< 11; i++) {
@@ -175,7 +177,7 @@ function createGrids() {
         }
         table.appendChild(row);
     }
-    grid.appendChild(table);
+//    grid.appendChild(table);
 
 }
 
@@ -196,12 +198,146 @@ document.getElementById("submit-btn-logout").addEventListener("click", function(
 })
 
 
-//$( "#testDrag" ).draggable();
-//$( "#B3" ).droppable({
-//  drop: function( event, ui ) {
-//    console.log('dropeed')
-//  }
-//});
+
+//drag/ drop part
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    let gridId = ev.srcElement.id;
+    let targetDom = document.getElementById(gridId);
+    console.log(createShipsLength(targetDom.firstChild.id));
+    targetDom.firstChild.remove();
+//    targetDom.style.backgroundColor = "blue";
+}
+
+function createShipsLength(shipType) {
+    if(shipType === "dragP") return 2;
+    if(shipType === "dragS" || shipType === "dragD") return 3;
+    if(shipType === "dragB") return 4;
+    else return 5;
+}
+
+//[
+//[false, false, false, false, false, false....],
+//[],........
+//]
+
+function generateGridArray() {
+    let grArr = [];
+
+    for(let i = 0; i< 10; i++) {
+        let rowArr = [];
+        for(let j = 0; j<10; j++) {
+            rowArr.push(false)
+        }
+        grArr.push(rowArr);
+    }
+    return grArr;
+}
+
+//console.log(generateGridArray());
+
+function updateGrid(rowId, colId, gridArr) {
+    let colNum = Number(colId) - 1;
+
+    if(!["A","B","C","D","E","F","G","H","I","J"].includes(rowId)) return ["row ID is WRONG!"]
+    if(!["1","2","3","4","5","6","7","8","9","10"].includes(colId)) return ["col ID is WRONG!"]
+
+    if(rowId === "A") gridArr[0][colNum] = true;
+    else if(rowId === "B") gridArr[1][colNum] = true;
+    else if(rowId === "C") gridArr[2][colNum] = true;
+    else if(rowId === "D") gridArr[3][colNum] = true;
+    else if(rowId === "E") gridArr[4][colNum] = true;
+    else if(rowId === "F") gridArr[5][colNum] = true;
+    else if(rowId === "G") gridArr[6][colNum] = true;
+    else if(rowId === "H") gridArr[7][colNum] = true;
+    else if(rowId === "I") gridArr[8][colNum] = true;
+    else if(rowId === "J") gridArr[9][colNum] = true;
+    return gridArr;
+}
+
+//console.log(updateGrid("A", "1", generateGridArray()));
+//console.log(updateGrid("D", "5", generateGridArray()));
+//console.log(updateGrid("C", "2", generateGridArray()));
+
+// updateShipLoc(["A1", "A2", "A3"], gridArr)
+
+function updateShipLoc(shipLocArr, gridArr) {
+
+    for(let loc of shipLocArr) {
+        let rowId = loc[0];
+        let colId = loc[1];
+
+        gridArr = updateGrid(rowId, colId, gridArr);
+    }
+    return gridArr;
+}
+
+console.log(updateShipLoc((["A1", "A2", "A3"]), generateGridArray()))
+
+function generateShipLoc(start, shipLength, shipDirection = "horizontal") {
+
+    let rowId = start[0]
+    let colId = start[1]
+    let shipLocArr = [];
+
+    if(shipDirection === 'horizontal') {
+        for(let i=0; i<shipLength; i++) {
+            let shipLoc = rowId + (Number(colId) + i);
+            shipLocArr.push(shipLoc)
+        }
+    }
+
+    else if(shipDirection === 'vertical') {
+        //A1, B1, C1
+        let rowArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+        let currentRowIndex = rowArr.indexOf(rowId) //2
+        for(let i=0; i<shipLength; i++) {
+            let shipLoc = rowArr[currentRowIndex + i] + colId;
+            shipLocArr.push(shipLoc);
+        }
+    }
+    return shipLocArr;
+}
+
+function colorGrid(gridArr) {
+    let rowArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    for(let i=0; i<gridArr.length;i++) {
+        for(let j=0; j<gridArr.length; j++) {
+            let columnId = j+1;
+            let rowId = rowArr[i];
+            let finalId = rowId + columnId
+            if(gridArr[i][j] === true) {
+               console.log(finalId)
+               document.getElementById(finalId).style.backgroundColor = "blue";
+
+            }
+            else {
+               document.getElementById(finalId).style.backgroundColor = "white";
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
